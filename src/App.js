227bfc84568a34f680ec7576881a8a3a47442c6d
porosh <jamilkashem2002@gmail.com>
2021-01-13 22:46:27 +0600

    BodyContent

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './App.css'
+import BodyContent from './Components/BodyContent/BodyContent'
 import Navigation from './Components/Navigation/Navigation'
 function App() {
   return (
     <div>
       <Navigation />
+      <BodyContent />
     </div>
   )
 }
---
+++ src/Components/BodyContent/BodyContent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function BodyContent() {
+    return (
+        <div className='BodyContent'>
+            <div className='bContentOne'></div>
+            <div className='bContenttwo'></div>
+        </div>
+    )
+}
+
+export default BodyContent
